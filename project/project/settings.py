0925c8256149330e46d2f39494970b2d9d2937c2
Zachary Herman <zach.herman33@gmail.com>
2012-10-07 00:06:51 -0400

    set up static/templates

--- project/project/settings.py
+++ project/project/settings.py
@@ -64,6 +64,8 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+	"/Users/Zachary/Documents/Hack-a-thon/ask-me-now/project/static"
+	"/home/oaguy1/git/AskMeNow/project/static"
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -103,6 +105,8 @@ ROOT_URLCONF = 'project.urls'
 WSGI_APPLICATION = 'project.wsgi.application'
 
 TEMPLATE_DIRS = (
+	"/Users/Zachary/Documents/Hack-a-thon/ask-me-now/project/templates"
+	"/home/oaguy1/git/AskMeNow/project/templates"
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
